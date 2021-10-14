66b86cef93b1f237a809854d224d573fbd05384e
Georgy Gorbenko <georgii.gorbenko@letmecode.org>
2021-10-14 16:07:56 +0300

    chore: created project

---
+++ WeatherApp/WeatherApp/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  WeatherApp
+//
+//  Created by Andrey Buksha on 14.10.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+
+}
+
